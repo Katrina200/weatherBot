3c7db0e68a916b1425dcf3100957b5531843c33c
Ekaterina Lysenko <baloven200@mail.ru>
2022-09-18 23:29:45 +0300

    Changed the whole code

--- anekdotparsing.py
+++ anekdotparsing.py
@@ -9,12 +9,9 @@ def parser (url):
     soup = b(r.text, 'html.parser')
     anekdots = soup.find_all('div', class_='text')
     return [c.text for c in anekdots]
-list_of_jokes = parser(URL)
-random.shuffle(list_of_jokes)
-#print(list_of_jokes)
-def jokes(message):
-    if message.text.lower() in '123456789':
-        bot.send_message(message.chat.id, list_of_jokes[0])
-        del list_of_jokes[0]
-    else:
-        bot.send_message(message.chat.id, 'Введите любую цифру: ')
+an = parser(URL)
+random.choice(an)
+
+
+
+
